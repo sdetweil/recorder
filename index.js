3831eabf9769569c66f517e292cf153347aaf67d
Sam Detweiler <sdetweil@gmail.com>
2020-06-11 21:07:07 -0500

    add reco feedback to visual display

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const io = require("socket.io-client");
 const EventEmitter = require("events");
 const { spawn } = require("child_process");
 const getPort = require("get-port");
-
-console.log("our location=" + __dirname);
+const kws_active = false;
+const redebug = false;
+if (redebug) console.log("our location=" + __dirname);
 function recorder(smart_mirror_remote_port) {
 	this.host = "http://localhost";
 	this.sm_port = smart_mirror_remote_port + 1;
@@ -20,14 +21,16 @@ function recorder(smart_mirror_remote_port) {
 
 	this.init = function () {
 		this.ioClient.on("connected", (socket) => {
-			//console.log("connected")
+			//if(redebug)
+			console.log("connected");
 		});
 
 		// background sonus process sends its config info
 		// so we don't have to figure out differently
 		this.ioClient.on("info", (info) => {
 			// indicate we have no hotwords
-			//console.log("got info");
+			//if(redebug)
+			console.log("got info");
 
 			this.startSonus(this.port);
 			this.ready = true;
@@ -37,63 +40,88 @@ function recorder(smart_mirror_remote_port) {
 		this.ioClient.on("stopped", () => {
 			// background task paused
 			// start our reco engine
-			//console.log("background recorder stopped, start ours")
+			//if(redebug)
+			console.log("background recorder stopped, start ours");
 			this.voiceClient.emit("start");
 			this.recording = true;
 		});
 
 		this.ioClient.on("started", () => {
-			//console.log("native reco started");
+			if (redebug) console.log("native reco started");
 		});
 	};
 
 	this.waitSocket = function (socket) {
 		var self = this;
-		//console.log("enter wait socket="+socket)
+		if (redebug) console.log("enter wait socket=" + socket);
 		if (!this.voiceClient) {
-			//console.log("host="+this.host + ":" +socket)
+			if (redebug) console.log("host=" + this.host + ":" + socket);
 			this.voiceClient = io.connect(this.host + ":" + socket);
-			//console.log("connecting to the voice client socket ="+socket)
+			if (redebug)
+				console.log("connecting to the voice client socket =" + socket);
 			this.timer = setTimeout(function () {
 				self.waitSocket(socket);
 			}, 500);
 		} else {
-			//console.log("have the voice client socket")
+			if (redebug) console.log("have the voice client socket");
 			clearTimeout(this.timer);
 			// tell waiting open task
-			this.voiceClient.on("text", (message) => {
-				//console.log("received text from reco engine="+message)
-				this.Emitter.emit("text", message);
+			this.voiceClient.on("partial-text", (message) => {
+				if (redebug)
+					console.log(
+						"assistant received partial from reco engine=" + message
+					);
+				this.Emitter.emit("partial", message);
+				// turn off our reco engine
+			});
+			this.voiceClient.on("final-text", (message) => {
+				if (redebug)
+					console.log(
+						"assistant received text from reco engine=" + message
+					);
+				this.Emitter.emit("final", message);
 				// turn off our reco engine
 				this.voiceClient.emit("stop");
+			});
+			this.voiceClient.on("stopped", () => {
 				// turn back on the mirrors engine
-				this.ioClient.emit("start");
+				let self = this;
+				// 200ms quiet time
+				setTimeout(() => {
+					self.ioClient.emit("start");
+				}, 100);
 			});
 			this.resolve(this.Emitter);
 		}
 	};
 	this.startSonus = function (socketNumber) {
 		// Initilize the keyword spotter
-		//console.log("process starting in the background")
-		this.kwsProcess = spawn(
-			"node",
-			[__dirname + "/sonus.js", socketNumber],
-			{
-				detached: false,
-			}
-		);
-		this.kwsProcess.on("error", (err) => {
-			console.error("assistant spawn err: ", err);
-		});
-		this.kwsProcess.on("exit", (code, signal) => {
-			if (code) {
-				console.error("assistant Child exited with code", code);
-			} else if (signal) {
-				console.error("assistant Child was killed with signal", signal);
-			} else {
-				console.log("assistant Child exited okay");
-			}
-		});
+		//if(redebug)
+		console.log("process starting in the background");
+		if (kws_active) {
+			this.kwsProcess = spawn(
+				"node",
+				[__dirname + "/sonus.js", socketNumber],
+				{
+					detached: false,
+				}
+			);
+			this.kwsProcess.on("error", (err) => {
+				console.error("assistant spawn err: ", err);
+			});
+			this.kwsProcess.on("exit", (code, signal) => {
+				if (code) {
+					console.error("assistant Child exited with code", code);
+				} else if (signal) {
+					console.error(
+						"assistant Child was killed with signal",
+						signal
+					);
+				} else {
+					if (redebug) console.log("assistant Child exited okay");
+				}
+			});
+		}
 		this.waitSocket(socketNumber);
 	};
 }
@@ -106,15 +134,18 @@ recorder.prototype.open = function () {
 		// tool to talk to our consumer
 		this.Emitter = new EventEmitter();
 
-		console.log("assistant requesting port");
+		if (redebug) console.log("assistant requesting port");
 		// ask for a range
 		var self = this;
 		getPort({ port: getPort.makeRange(5100, 5200) })
 			.then((port) => {
+				port = 5100;
 				// use first available
-				console.log("assistant have available ports =", port);
+				if (redebug)
+					console.log("assistant have available ports =", port);
 				self.port = port;
-				console.log("assistant have available port=" + self.port);
+				if (redebug)
+					console.log("assistant have available port=" + self.port);
 
 				// io client to background sonus
 				self.ioClient = io.connect(self.host + ":" + this.sm_port);
@@ -134,7 +165,8 @@ recorder.prototype.open = function () {
 				self.recording = false;
 			})
 			.catch((error) => {
-				console.log("assistant port request failed=" + error);
+				if (redebug)
+					console.log("assistant port request failed=" + error);
 			});
 	});
 };
@@ -146,11 +178,13 @@ recorder.prototype.start = function () {
 };
 // not sure what to do here
 recorder.prototype.stop = function () {
-	//console.log("stop");
+	//if(redebug)
+	console.log("stop");
 };
 // not sure what to do here
 recorder.prototype.close = function () {
-	//console.log("close");
+	//if(redebug)
+	console.log("close");
 };
 // are we recording ?
 recorder.prototype.recording = function () {
